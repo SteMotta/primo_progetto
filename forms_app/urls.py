f38fc6fc29720fc648848136fea090c1bcae6f66
Stefano Motta <103846397+SteMotta@users.noreply.github.com>
2026-03-09 13:36:56 +0100

    Fine passo 23

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
+import forms_app
 from forms_app.views import *
 app_name = "forms_app"
 urlpatterns = [
     path('contattaci/', contatti, name="contatti"),
     path('lista_contatti', lista_contatti, name="lista_contatti"),
+    path('elimina_contatto/<int:pk>', elimina_contatto, name="elimina_contatto"),
+    path('modifica_contatto/<int:pk>', modifica_contatto, name="modifica_contatto"),
 ]
--- forms_app/views.py
+++ forms_app/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import FormContatto
 from .models import Contatto
+from django.shortcuts import get_object_or_404,redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 def contatti(request):
     if request.method == 'POST':
         form = FormContatto(request.POST)
@@ -26,3 +29,28 @@ def lista_contatti(request):
     all_contatti = Contatto.objects.all()
     context = {'contatti': all_contatti}
     return render(request, "lista_contatti.html", context)
+
+@login_required(login_url='/accounts/login/') # Server per rendere la view solo usabile da chi è autenticato
+def modifica_contatto(request, pk):
+    contatto = get_object_or_404(Contatto, pk=pk)
+
+    form = None
+    if request.method == 'GET':
+        form = FormContatto(instance=contatto)
+    if request.method == 'POST':
+        form = FormContatto(request.POST, instance=contatto)
+        if form.is_valid():
+            form.save()
+            return redirect('forms_app:lista_contatti')
+
+    context = {'form': form, 'contatto': contatto}
+    return render(request, "modifica_contatto.html", context)
+
+@staff_member_required(login_url='/accounts/login/') # Permette di cancellare solo a un utente admin
+def elimina_contatto(request, pk):
+    contatto = get_object_or_404(Contatto, pk=pk)
+    if request.method == 'POST':
+        contatto.delete()
+        return redirect('forms_app:lista_contatti')
+    context = {'contatto': contatto}
+    return render(request, 'elimina_contatto.html', context)
